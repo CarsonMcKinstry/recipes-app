8db888c77b4c66274bd21385fd56bd24975d8417
Carson McKinstry <carson.mckinstry@gmail.com>
2017-12-04 19:50:31 -0600

    Removed unneeded code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,10 @@
 import authRouter from "./auth";
 import recipesRouter from "./recipes";
-import { requireAuth } from "../helpers/passportHelper";
 import { errors } from "celebrate";
 
 export default (app, passport) => {
-  const authMiddleware = requireAuth(passport);
-
   authRouter(app, passport);
   recipesRouter(app, passport);
 
   app.use(errors());
-
-  app.get("/", authMiddleware, (req, res) => {
-    res.send("Change me!");
-  });
 };
